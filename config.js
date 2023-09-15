95fba1dcc05bc16b1cb12a29af4a33fc5dfa1371
Gabriel Contreras <gabriel.contreras@gmail.com>
2023-09-15 00:10:29 -0500

    add basic use of environments with dotenv and mongodb atlas uri

---
+++ config.js
@@ -0,0 +1,4 @@
+
+const ENV = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI_ATLAS = process.env.MONGODB_URI_ATLAS || 'mongodb://localhost:27017/turbotraffic';
